65b2edcafbcdc7950eb7d768c873b4c4aa7a7c0d
AlirezaHoosh <alijgh003@gmail.com>
2024-04-05 00:19:12 +0330

    [ADD] myfirstuserprogram.c has been added and tested

---
+++ user/myfirstuserprogram.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int 
+main(int argc , char *argv[])
+{
+    printf("this is my first xv6 user program.\n");
+    exit(0);
+}
